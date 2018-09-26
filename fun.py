6174281255874e4beaa057970c56d51756c88095
immortalshadow007 <42990140+immortalshadow007@users.noreply.github.com>
2018-09-26 13:44:28 +0530

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,21 +1,19 @@
 a=int(input("Enter the value of A: "))
 b=int(input("Enter the value of B: "))
-p=input("what do you want to do? +,-,*,/: ")
+p=int(input("what do you want to do? 1,2,3,4: "))
 def add():
-	return a+B
-def sub(a,b):
+	return a+b
+def sub():
 	return a-b
-def mul(a,b):
+def mul():
 	return a*b
-def divide(a,b):
+def divide():
 	return a/b
-
-if(p=='+'):
-	print("addition=",add(a,b))
-elif(p=='-'):
-	print("subtraction=",sub(76,78))
-elif(p=='*'):
-	print("multiplication=",mul(a,b))
-elif(p=='/'):
-	print("divide=",divide(a,b))
-
+if(p==1):
+	print(" addition= ",add())
+if(p==2):
+	print(" subtraction= ",sub())
+if(p==3):
+	print(" multiplication= ",mul())
+if(p==4):
+	print("Division= ",divide())
