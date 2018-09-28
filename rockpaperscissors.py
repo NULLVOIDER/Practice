d5046881b729495be5f920bf0f9e900d86784385
immortalshadow007 <42990140+immortalshadow007@users.noreply.github.com>
2018-09-28 09:41:50 +0530

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,50 +1,51 @@
 import random
 
-print("Hello! Welcome to the Rock Paper Scissors Program!")
+print("WELCOME TO LEGEND OF ALL GAMES,THE ROCK PAPER SCISSORS")
 
 isWin = False
-
+PlayerInput=0
 while isWin == False:
     print("")
     print("Press 1 for Rock.")
     print("Press 2 for Paper.")
     print("Press 3 for Scissors.")
+    while(PlayerInput==0)  :
+        PlayerInput = int(input("What would you like to play?"))
 
-    UserInput = int(input("What would you like to play?"))
     ComputerInput = random.randint(1,3)
 
-    if (UserInput == 1) and (ComputerInput == 1):
+    if (PlayerInput == 1) and (ComputerInput == 1):
         isWin = False
         print("It's a draw; you both played Rock!")
         
-    if (UserInput == 2) and (ComputerInput == 1):
+    if (PlayerInput == 2) and (ComputerInput == 1):
         isWin == True
-        print("You win! The computer played Rock!")
+        print("You win,  computer played Rock!")
 
-    if (UserInput == 3) and (ComputerInput == 1):
+    if (PlayerInput == 3) and (ComputerInput == 1):
         isWin == True
-        print("You lose! The computer played Rock!")
+        print("You lose, computer played Rock!")
         
-    if (UserInput == 1) and (ComputerInput == 2):
+    if (PlayerInput == 1) and (ComputerInput == 2):
         isWin = True
-        print("You lose! The computer played Paper!")
+        print("You lose, computer played Paper!")
         
-    if (UserInput == 2) and (ComputerInput == 2):
+    if (PlayerInput == 2) and (ComputerInput == 2):
         isWin == False
         print("It's a draw; the computer played Paper!")
 
-    if (UserInput == 3) and (ComputerInput == 2):
+    if (PlayerInput == 3) and (ComputerInput == 2):
         isWin == True
-        print("You win! The computer played Paper!")
+        print("You win, computer played Paper!")
         
-    if (UserInput == 1) and (ComputerInput == 3):
+    if (PlayerInput == 1) and (ComputerInput == 3):
         isWin = True
-        print("You win! The computer played Scissors!")
+        print("You win, computer played Scissors!")
         
-    if (UserInput == 2) and (ComputerInput == 3):
+    if (PlayerInput == 2) and (ComputerInput == 3):
         isWin == True
-        print("You lose! The computer played Scissors!")
+        print("You lose, computer played Scissors!")
 
-    if (UserInput == 3) and (ComputerInput == 3):
+    if (PlayerInput == 3) and (ComputerInput == 3):
         isWin == False
-        print("It's a draw; the computer played Scissors!")
+        print("It's a draw; computer played Scissors!")
