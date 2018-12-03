ede80ddc89f769f216b5dd72435b3f6f54de2020
immortalshadow007 <42990140+immortalshadow007@users.noreply.github.com>
2018-12-03 14:26:56 +0530

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,41 +1,74 @@
-import tensorflow as tf 
-T, F = 1.0, -1.0
-bias = 1.0
-training_input = [
-    [T, T, bias],
-    [T, F, bias],
-    [F, T, bias],
-    [F, F, bias],
-]
-training_output = [
-    [T],
-    [F],
-    [F],
-    [F],
-]
-w = tf.Variable(tf.random_normal([3, 1]), dtype=tf.float32)
-def step(x):
-    is_greater = tf.greater(x, 0)
-    as_float = tf.to_float(is_greater)
-    doubled = tf.multiply(as_float, 2)
-    return tf.subtract(doubled, 1)
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
 
-output = step(tf.matmul(training_input, w))
-error = tf.subtract(training_output, output)
-mse = tf.reduce_mean(tf.square(error))
-
-delta = tf.matmul(training_input, error, transpose_a=True)
-train = tf.assign(w, tf.add(w, delta))
-
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-
-err, target = 1, 0
-epoch, max_epochs = 0, 10
-
-while err > target and epoch < max_epochs:
-    epoch += 1
-    err, _ = sess.run([mse, train])
-print('epoch:', epoch, 'mse:', err)
-
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+while True:
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+	net.setInput(blob)
+	detections = net.forward()
+for i in np.arange(0, detections.shape[2]):
+		confidence = detections[0, 0, i, 2]
+ 
+		if confidence > args["confidence"]:
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+			label = "{}: {:.2f}%".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+			cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			break
+	fps.update()
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+cv2.destroyAllWindows()
+vs.stop()
 print(sess.run(w))
